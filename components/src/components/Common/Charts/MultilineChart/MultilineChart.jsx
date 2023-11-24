63202c900366206bb2fddc0bc5e891a7c01a1cf6
JCaeta <63880187+JCaeta@users.noreply.github.com>
2023-11-24 15:03:38 -0300

    Fixing packages names

---
+++ components/src/components/Common/Charts/MultilineChart/MultilineChart.jsx
@@ -0,0 +1,24 @@
+// import styles from './MultilineChart.module.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { } from '@mui/material'
+
+export const MultilineChart = props => {
+
+    return (<>
+
+    </>);
+}
+
+MultilineChart.defaultProps =
+{
+
+
+}
+
+MultilineChart.propTypes =
+{
+
+}
+
+
